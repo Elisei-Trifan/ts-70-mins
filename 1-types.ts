f88778f7c2d6fc6c9c09649c41ba5ffecd62be84
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-26 15:24:32 +0200

    new dates

--- 1-types.ts
+++ 1-types.ts
@@ -8,3 +8,17 @@ const myArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
 
 myArray.push(15)
 console.log(myArray)
+
+// Tuple - кортеж
+const contact: [string, number] = ['Elisei', 123456789]
+
+// Any
+let age: any = 29
+age = '229'
+
+//  ===
+function sayName(name: string): void {
+  console.log(name)
+}
+
+sayName('Elisei')
