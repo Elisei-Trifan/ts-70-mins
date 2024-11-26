7e14eed23aa3cd088d08bf76f5c0df5874f4423f
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-26 14:53:55 +0200

    new dir

---
+++ 1-types.ts
@@ -0,0 +1,10 @@
+const str: string = 'Hello'
+
+let int: number = 42
+int = 22
+
+const myArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const myArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+myArray.push(15)
+console.log(myArray)
