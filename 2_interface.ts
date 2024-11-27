11c6f473e824257337f4d88b5527bb95f840ea10
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-27 10:24:58 +0200

    динамические ключи и их типы

--- 2_interface.ts
+++ 2_interface.ts
@@ -24,3 +24,46 @@ const rectangle2: Rect = {
 }
 
 rectangle1.color = 'green'
+
+const rectangle3 = {} as Rect // Пока массив пустой, но позже он должен соответсвовать типу Rect
+
+// ==================
+interface RectWithArea extends Rect {
+  getArea: () => number
+}
+
+const rectangle4: RectWithArea = {
+  id: '125',
+  size: {
+    width: 20,
+    height: 30,
+  },
+  getArea() {
+    return this.size.width * this.size.height
+  },
+}
+
+//  ===============
+
+interface IClock {
+  time: Date
+  setTime(date: Date): void
+}
+
+class Clock implements IClock {
+  time: Date = new Date()
+  setTime(date: Date): void {
+    this.time = date
+  }
+}
+
+// ==============
+
+interface Styles {
+  [key: string]: string
+}
+
+const css: Styles = {
+  border: '1px solid red',
+  marginTop: '20px',
+}
